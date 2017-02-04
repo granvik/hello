25267405159e5e61c0b93a7fcbd449ba4098677c
aradm <aradm@aradm.ru>
2017-02-04 20:24:01 +0300

    Проверка

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: granvik
+ * Date: 04.02.17
+ * Time: 20:14
+ */
+?>
+<html>
+<head><title>test</title></head>
+<body>
+<?php
+echo "<p>Hello World!";
+echo "<p>Привет, Мир!";
+?>
+</body>
+</html>
